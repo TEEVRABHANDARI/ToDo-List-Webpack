05ed9d44a5693f11ddcd15a5825b151bb0264bbf
Teevra Bhandari <teevrabhandari2003@gmail.com>
2024-03-17 23:52:23 +0530

    Basic webpack file structure done

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+console.log("Testing");
+
+const contentDiv = document.querySelector("#content")
+const heading = document.createElement("h1")
+heading.textContent = "To-Do list"
+contentDiv.appendChild(heading)
